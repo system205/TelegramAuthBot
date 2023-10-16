cdbf42008013ba0c1d47bca564ec8d78188d19d7
system205 <arsenmutalapov205@gmail.com>
2023-10-16 22:48:59 +0300

    init spring initializer

---
+++ src/main/java/com/system205/telegramauthbot/TelegramAuthBotApplication.java
@@ -0,0 +1,13 @@
+package com.system205.telegramauthbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramAuthBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramAuthBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/system205/telegramauthbot/TelegramAuthBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system205.telegramauthbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramAuthBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
