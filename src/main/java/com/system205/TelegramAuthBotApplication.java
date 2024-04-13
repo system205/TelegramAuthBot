d09c1827b507ec031170aace232cab68ccf32155
system205 <arsenmutalapov205@gmail.com>
2024-04-13 23:24:32 +0300

    clean main method extracting to command line runner

--- src/main/java/com/system205/TelegramAuthBotApplication.java
+++ src/main/java/com/system205/TelegramAuthBotApplication.java
@@ -1,28 +1,36 @@
 package com.system205;
 
-import lombok.extern.slf4j.*;
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.context.*;
-import org.telegram.telegrambots.meta.*;
-import org.telegram.telegrambots.meta.exceptions.*;
-import org.telegram.telegrambots.meta.generics.*;
-import org.telegram.telegrambots.updatesreceivers.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.generics.LongPollingBot;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-@SpringBootApplication
 @Slf4j
+@SpringBootApplication
 public class TelegramAuthBotApplication {
 
     public static void main(String[] args) {
-        ApplicationContext context = SpringApplication.run(TelegramAuthBotApplication.class, args);
-
-        try {
-            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-            LongPollingBot bot = context.getBean(LongPollingBot.class);
-            botsApi.registerBot(bot);
-            log.info("Bot is registered successfully");
-        } catch (TelegramApiException e) {
-            log.error("Can't register a bot", e);
-        }
+        SpringApplication.run(TelegramAuthBotApplication.class, args);
     }
+
+    @Bean
+    CommandLineRunner commandLineRunner(ApplicationContext context) {
+        return (String... args)-> {
+            try {
+                TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+                botsApi.registerBot(context.getBean(LongPollingBot.class));
+                log.info("Bot is registered successfully");
+            } catch (TelegramApiException e) {
+                log.error("Can't register a bot", e);
+            }
+        };
+    }
+
 }
+
